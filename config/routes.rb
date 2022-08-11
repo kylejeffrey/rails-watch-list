43eaa0ac292c1e25e080f970da284565fc03d7cb
Kyle Jeffrey <kyle.m.jeffrey@gmail.com>
2022-08-11 16:14:47 +0900

    implemented lists controller and all but the #destroy in bookmarks controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,23 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # A user can see all the lists
+  # lists#index
+  # get '/lists', to: 'lists#index', as: :lists
+
+  # A user can see the details of a given list and its name
+  # lists#show
+  # get '/lists/:id', to: 'lists#show', as: :list
+
+  # A user can create a new list
+  # lists#new
+  # lists#create
+  # get '/lists/new', to: 'lists#new', as: :new_list
+  # post '/lists', to: 'lists#create'
+
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
