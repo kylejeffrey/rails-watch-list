1e7324d6a91ab7957fd1624add9c459824f2e65a
Kyle Jeffrey <kyle.m.jeffrey@gmail.com>
2022-08-12 16:39:57 +0900

    added conditonal helper for index movie poster urls

--- app/helpers/lists_helper.rb
+++ app/helpers/lists_helper.rb
@@ -1,2 +1,11 @@
 module ListsHelper
+  def get_url(list)
+    if  list.photo.attached?
+      cl_image_path(list.photo.key)
+    elsif list.bookmarks.any?
+       list.bookmarks.first.movie.poster_url
+    else
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPQDYau3Hs4-xw1i8jVSUY4BlF4FLmg8lQqg&usqp=CAU'
+    end
+  end
 end
