fc2253228802fb5f0283b3d55b1612f169baf1df
Kyle Jeffrey <kyle.m.jeffrey@gmail.com>
2022-08-14 16:41:25 +0900

    made typed.js speed slower

--- app/javascript/controllers/typed_js_controller.js
+++ app/javascript/controllers/typed_js_controller.js
@@ -1,11 +1,12 @@
 import { Controller } from "@hotwired/stimulus"
+import Typed from "typed.js"
 
 // Connects to data-controller="typed-js"
 export default class extends Controller {
   connect() {
     new Typed(this.element, {
       strings: ["Change your face", "To Nicholas Cage's face"],
-      typeSpeed: 50,
+      typeSpeed: 10,
       loop: true
     })
   }
